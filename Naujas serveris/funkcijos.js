ca752734dd0ad2fe418bf5e9a2415b6eccb6c296
Algis1978 <86305896+Algis1978@users.noreply.github.com>
2021-09-07 11:12:26 +0300

    min

---
+++ Naujas serveris/funkcijos.js
@@ -0,0 +1,11 @@
+function suma (a, b) {
+    return a+b;
+}
+function skirtumas (a, b) {
+    return a-b;
+}
+
+export {
+    suma,
+    skirtumas
+}
---
+++ Naujas serveris/naujas-serveris.js
@@ -0,0 +1,4 @@
+ import moment from "moment";
+ const m = moment ();
+ m.add(47, "month");
+ console.log (m.format ("YYYY-MM-DD"));
