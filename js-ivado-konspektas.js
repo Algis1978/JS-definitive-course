994a01dcbd647a40b6e48c7995d781b86356ffec
Algis1978 <86305896+Algis1978@users.noreply.github.com>
2021-09-07 16:06:04 +0300

    min

--- js-ivado-konspektas.js
+++ js-ivado-konspektas.js
@@ -89,6 +89,8 @@ let knyga2 = {
 }
 delete knyga2["autorius"];
 console.log(knyga2);
+//.hasOwnProperty metodas patikrina, ar objekte yra tam tikro pavadinimo savybė:
+console.log (knyga2.hasOwnProperty("autorius"));//false (ne).
 
 //Su raktažodžiu "new" galima sukurti objekto tipo tekstus, skaičius ir boolean vertes:
 let z = new String ("niekada")
@@ -787,3 +789,49 @@ console.log(h);
 
 //Įvestą funkciją galima iššaukti bet kurioje bloko vietoje. Įveskite 1-oje kodo eilutėje console.log (blokoNaudojimoPavyzdys2 (40, 4)); ir rezultatas bus pavaizduotas konsolėje nepaisant to, kad pati funkcija aprašyta vėlesnėse eilutėse.
 //Vengtina kurti to paties pavadinimo ar globalaus objekto pavadinimo funkcijas, metodus. 
+
+// Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+console.log (contacts.length);
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+for (var i = 0; i<contacts.length; i++){
+    if (contacts[i]["firstName"]=name&&contacts[i][prop]) {
+        return contacts[i][prop];
+    }
+    else  {
+
+
+    }
+}
+  // Only change code above this line
+}
+
+console.log (lookUpProfile("Akira", "likes"));
+console.log (lookUpProfile("Sherlock", "likes"));
